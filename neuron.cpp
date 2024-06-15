a323d2d1d0074b2c13d02d41cbec56958b02c232
JBank25 <urbankjoshua1@gmail.com>
2024-06-15 13:36:01 -0700

    Renaming engine

--- neuron.cpp
+++ neuron.cpp
@@ -2,7 +2,7 @@
 #include <random>
 #include <vector>
 
-#include "engine.h"
+#include "neuron.h"
 #include "value.h"
 
 using namespace std;
@@ -14,16 +14,16 @@ Neuron::Neuron(int numInputs)
     weights.resize(numInputs); // Resize the weights vector to match the number of inputs
     for (unsigned int i = 0; i < numInputs; i++)
     {
-        // weights[i] = Value(1);
+        weights[i] = Value(distribution(generator));
     }
-    bias = Value(1);
+    bias = Value(distribution(generator));
 }
 int main()
 {
     // Value testVal = Value(1.0);
-    // Neuron testNeuron = Neuron(10);
-    // for (int i = 0; i < testNeuron.numInputs; i++)
-    // {
-    //     std::cout << testNeuron.weights[i].data << std::endl;
-    // }
+    Neuron testNeuron = Neuron(10);
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << testNeuron.weights[i].data << std::endl;
+    }
 }
